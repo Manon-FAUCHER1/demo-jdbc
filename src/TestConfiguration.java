e936b36678b9223330fff6e1cedbcaec4b4d390f
manon <manon.faucher.pro@gmail.com>
2021-02-19 10:10:06 +0100

    first commit

---
+++ src/TestConfiguration.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author manon
+ *
+ */
+public class TestConfiguration {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
